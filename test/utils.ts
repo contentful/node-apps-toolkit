914a2e4b2d980c24899d02b9208f8c49f893b3a5
Manuel Spagnolo <spagnolo.manu@gmail.com>
2020-10-02 12:58:51 +0200

    test: delete old keys before creating a new one

--- test/utils.ts
+++ test/utils.ts
@@ -4,22 +4,36 @@ import base64url from 'base64url'
 
 import { createHttpClient, createValidateStatusCode } from '../src/utils'
 
-const http = createHttpClient()
+const http = createHttpClient({
+  headers: {
+    Authorization: `Bearer ${process.env.PERSONAL_ACCESS_TOKEN}`,
+  },
+})
 
-export const setPublicKey = (publicKey: Buffer) => {
+export const cleanOldKeys = async () => {
   const organizationId = process.env.ORGANIZATION_ID
   const appDefinitionId = process.env.APP_ID
-  const keyId = base64url(
-    crypto
-      .createHash('sha256')
-      .update(publicKey)
-      .digest()
+
+  const { body } = await http.get(
+    `organizations/${organizationId}/app_definitions/${appDefinitionId}/keys`
   )
+  const fingerprints = JSON.parse(body).items.map((i: any) => i.jwk.x5t)
+
+  const deleteKeysRequests = fingerprints.map((fingerprint: string) => {
+    return http.delete(
+      `organizations/${organizationId}/app_definitions/${appDefinitionId}/keys/${fingerprint}`
+    )
+  })
+
+  return Promise.all(deleteKeysRequests)
+}
+
+export const setPublicKey = async (publicKey: Buffer) => {
+  const organizationId = process.env.ORGANIZATION_ID
+  const appDefinitionId = process.env.APP_ID
+  const keyId = base64url(crypto.createHash('sha256').update(publicKey).digest())
 
   return http.post(`organizations/${organizationId}/app_definitions/${appDefinitionId}/keys`, {
-    headers: {
-      Authorization: `Bearer ${process.env.PERSONAL_ACCESS_TOKEN}`
-    },
     json: {
       jwk: {
         kty: 'RSA',
@@ -27,11 +41,11 @@ export const setPublicKey = (publicKey: Buffer) => {
         alg: 'RS256',
         x5c: [publicKey.toString('base64')],
         kid: keyId,
-        x5t: keyId
-      }
+        x5t: keyId,
+      },
     },
     hooks: {
-      afterResponse: [createValidateStatusCode([201, 200])]
-    }
+      afterResponse: [createValidateStatusCode([201, 200])],
+    },
   })
 }
