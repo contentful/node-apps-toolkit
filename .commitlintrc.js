e985ec56345b362ca89369e01d812d2835e386fc
Manuel Spagnolo <shikaan@users.noreply.github.com>
2022-03-25 09:40:34 +0100

    chore: skip autogenerated messages in commitlint (#45)

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,4 +1,7 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
-  ignores: [(msg) => msg.includes('Signed-off-by: dependabot[bot]')],
+  ignores: [
+    (msg) => msg.includes('Signed-off-by: dependabot[bot]'),
+    (msg) => msg.includes('chore(release):'),
+  ],
 }
