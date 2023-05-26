2fa69ea4949194085c1bade22dee4eb4d969060c
Kado <kdamball@users.noreply.github.com>
2023-05-26 15:56:24 +0200

    feat: add new action call context (#240)

--- src/requests/index.ts
+++ src/requests/index.ts
@@ -2,6 +2,7 @@ export { signRequest } from './sign-request'
 export { verifyRequest } from './verify-request'
 export { ContentfulHeader, ContentfulContextHeader } from './typings'
 export type {
+  AppActionCallContext,
   CanonicalRequest,
   SignedRequestWithContextHeadersWithApp,
   SignedRequestWithContextHeadersWithUser,
