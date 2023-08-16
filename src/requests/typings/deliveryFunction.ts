cf91e214b98fb2c7779f4c5c2ecbf19d4c54b681
Bas Kiers <kiers.bas@gmail.com>
2023-08-16 13:11:56 +0200

    Add types for deliveryFunction implementation [MONET-1334] (#313)
    * refactor: split typings.ts into multiple files
    
    * feat: add deliveryFunction types
    
    * feat: remove not yet decided upon values from interface

---
+++ src/requests/typings/deliveryFunction.ts
@@ -0,0 +1,62 @@
+// Remove when this eslint rule covers all the cases
+// https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/ROADMAP.md
+/*eslint-disable no-unused-vars*/
+
+export enum DeliveryFunctionRequestEventType {
+  GRAPHQL_FIELD_MAPPING = 'graphql.field.mapping',
+  GRAPHQL_QUERY = 'graphql.query',
+}
+
+export type GraphQLFieldTypeMappingRequest = {
+  fields: { contentTypeId: string; field: Field }[]
+}
+
+type Field = {
+  id: string
+  type: string
+}
+
+export type GraphQLFieldTypeMapping = {
+  contentTypeId: string
+  fieldId: string
+  graphQLOutputType: string
+  graphQLQueryField: string
+  graphQLQueryArgument: string
+}
+
+export type GraphQLFieldTypeMappingResponse = GraphQLFieldTypeMapping[]
+
+export type GraphQLQueryRequest = {
+  query: string
+  variables: Record<string, unknown>
+  operationName?: string
+}
+
+/**
+ * @see https://spec.graphql.org/October2021/#sec-Response
+ */
+export type GraphQLQueryResponse = {
+  data?: Record<string, any> | null
+  errors?: readonly Record<string, any>[]
+  extensions?: Record<string, unknown>
+}
+
+/**
+ * P: Possibility to type app installation parameters
+ */
+export type DeliveryFunctionEventContext = {
+  spaceId: string
+  environmentId: string
+  appInstallationParameters: Record<string, any>
+}
+
+export type DeliveryFunctionEventHandlers = {
+  [DeliveryFunctionRequestEventType.GRAPHQL_FIELD_MAPPING]: (
+    event: GraphQLFieldTypeMappingRequest,
+    context: DeliveryFunctionEventContext
+  ) => Promise<GraphQLFieldTypeMappingResponse>
+  [DeliveryFunctionRequestEventType.GRAPHQL_QUERY]: (
+    event: GraphQLQueryRequest,
+    context: DeliveryFunctionEventContext
+  ) => Promise<GraphQLQueryResponse>
+}
