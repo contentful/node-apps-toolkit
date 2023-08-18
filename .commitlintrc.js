46ad15e8174ee701ac5e6069cf6f0f47af2f1af1
Andi Pätzold <andreas.paetzold@contentful.com>
2023-08-18 12:28:09 +0200

    chore: remove commitlint (#317)

--- .commitlintrc.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  extends: ['@commitlint/config-conventional'],
-  ignores: [
-    (msg) => msg.includes('Signed-off-by: dependabot[bot]'),
-    (msg) => msg.includes('chore(release):'),
-  ],
-}
