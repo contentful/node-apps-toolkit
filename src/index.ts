7227b5a431ac9f90933d7538be96f76b3f814b18
Tyler Collins <tyler.collins@contentful.com>
2025-03-06 09:59:18 -0700

    fix: export all types

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,2 @@
 export { getManagementToken } from './keys'
-export { signRequest, verifyRequest, ContentfulHeader, ExpiredRequestException } from './requests'
-
-export type {
-  AppEventRequest,
-  AppEventPayloadMap,
-  AppActionCallContext,
-  CanonicalRequest,
-  SignedRequestHeaders,
-  FunctionEventContext,
-  FunctionEventHandler,
-  FunctionEventType,
-  FunctionEvent,
-  FunctionTypeEnum,
-} from './requests'
+export * from './requests'
--- src/requests/index.ts
+++ src/requests/index.ts
@@ -1,19 +1,4 @@
 export { ExpiredRequestException } from './exceptions'
 export { signRequest } from './sign-request'
 export { verifyRequest } from './verify-request'
-export { ContentfulHeader, ContentfulContextHeader } from './typings'
-export type {
-  AppEventRequest,
-  AppEventPayloadMap,
-  AppActionCallContext,
-  CanonicalRequest,
-  FunctionEvent,
-  FunctionEventContext,
-  FunctionEventHandler,
-  FunctionEventType,
-  FunctionTypeEnum,
-  SignedRequestWithContextHeadersWithApp,
-  SignedRequestWithContextHeadersWithUser,
-  SignedRequestWithoutContextHeaders,
-  SignedRequestHeaders,
-} from './typings'
+export * from './typings'
