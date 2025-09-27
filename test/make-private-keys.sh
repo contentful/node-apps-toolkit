76069fb1749ccd17275ad10fa0c6aab063773cbe
Bob Hemphill <robert.hemphill@contentful.com>
2025-09-27 14:21:30 -0600

    test: logging for keys

--- test/make-private-keys.sh
+++ test/make-private-keys.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env sh
 
+pwd
 mkdir -p keys
 
 openssl req -x509 -newkey rsa:4096 -nodes -batch -noout -keyout keys/key.pem
 openssl rsa -in keys/key.pem -pubout -outform DER -out keys/key.der.pub
+
+ls -la keys
