4344b31355838b547f23d7588c9e60bb671e3308
Bob Hemphill <robert.hemphill@contentful.com>
2025-09-27 14:25:05 -0600

    test: logging for keys

--- src/keys/get-management-token.spec.ts
+++ src/keys/get-management-token.spec.ts
@@ -14,6 +14,7 @@ import { Logger } from '../utils'
 import { SignJWT } from 'jose'
 import { LRUCache } from 'lru-cache'
 
+console.log('keys', path.join(__dirname, '..', '..', 'keys', 'key.pem'))
 const PRIVATE_KEY = fs.readFileSync(path.join(__dirname, '..', '..', 'keys', 'key.pem'), 'utf-8')
 const APP_ID = 'app_id'
 const SPACE_ID = 'space_id'
